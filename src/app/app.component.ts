c342097ced207d30b099b11c79dd732358414dc5
mingh4hire <mingh4hire@gmail.com>
2020-04-24 23:08:57 -0500

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component } from "@angular/core";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular';
-  fg;
-  constructor(fb: FormBuilder){
-    this.fg  = fb.group({a: [null, Validators.pattern('abcd[1-9]') ],
-      b: [null, Validators.required]});
-    
+export class AppComponent {
+  name = "Angular";
+  fg: FormGroup;
+  constructor(fb: FormBuilder) {
+    this.fg = fb.group(
+      {
+        a: [null, Validators.pattern("abcd[1-9]")],
+        b: [null, Validators.required]
+      },
+      (fgg: FormGroup) => {
+        fgg.setErrors({ a: 23423 });
+      }
+    );
   }
 }
